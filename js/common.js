04f911ce8f9c3e70936a5b573b0c13d89d2186c7
NiyazovProject <32863115+NiyazovProject@users.noreply.github.com>
2017-10-18 10:51:37 +0300

    аа

--- js/common.js
+++ js/common.js
@@ -1,58 +1,4 @@
-//menu-slide-toogle//
-$(document).ready(function() {
-  $('.menu-link').click(function() {
-    $('.nav').slideToggle(300);
-  });//end slide toggle
-  
-  $(window).resize(function() {		
-    if (  $(window).width() > 300 ) {			
-     $('.nav').removeAttr('style');
-   }
- });
-
-});
-
-
-$(document).ready(function() {
-  $('.maps__btn').click(function() {
-    $('.maps__full').slideToggle(500);
-  });//end slide toggle
-  
-  $(window).resize(function() {    
-    if (  $(window).width() > 500 ) {     
-      $('.maps__full').removeAttr('style');
-    }
-  });
-
-});
-
-
-
-
-//menu-hamburg-toogle//
-$(document).ready(function() {
-	var link = $('.menu-link');
-	var link_active = $('.menu-link__active');
-
-	link.click(function(){
-    link.toggleClass('menu-link__active');
-    
-  });
-
-  link_active.click(function(){
-  	link.remoClass(menu-link_active);
-  })
-});
-
-
-//fixed-menu//
-var options = {
-  offset: 0
-}
-var header = new Headhesive('.header-wr',options);
-//fixed-menu-end//
-
-
+/*
 
 //дожидаемся полной загрузки страницы
 window.onload = function () {
@@ -69,4 +15,7 @@ window.onload = function () {
         return false;
       }
     }
+
+    */ 
+    
 
