405969c88e74e4239108da67bd35a4852a9ca751
NiyazovProject <32863115+NiyazovProject@users.noreply.github.com>
2017-10-18 10:49:02 +0300

    аа

---
+++ img/js/common.js
@@ -0,0 +1,21 @@
+/*
+
+//дожидаемся полной загрузки страницы
+window.onload = function () {
+
+    //получаем идентификатор элемента
+    var a = document.getElementById('btn-text');
+    
+    //вешаем на него событие
+    a.onclick = function() {
+        //производим какие-то действия
+        if (this.innerHTML=='Развернуть карту') this.innerHTML = 'Свернуть карту';
+        else this.innerHTML = 'Развернуть карту';
+        //предотвращаем переход по ссылке href
+        return false;
+      }
+    }
+
+    */ 
+    
+
