39806dc3a34cedbe72b378a274906c3eb53ee5e8
NiyazovProject <32863115+NiyazovProject@users.noreply.github.com>
2017-10-30 14:12:42 +0300

    лл

--- js/common.js
+++ js/common.js
@@ -1,57 +1,31 @@
-/*
-
-//дожидаемся полной загрузки страницы
-window.onload = function () {
-
-    //получаем идентификатор элемента
-    var a = document.getElementById('btn-text');
-    
-    //вешаем на него событие
-    a.onclick = function() {
-        //производим какие-то действия
-        if (this.innerHTML=='Развернуть карту') this.innerHTML = 'Свернуть карту';
-        else this.innerHTML = 'Развернуть карту';
-        //предотвращаем переход по ссылке href
-        return false;
-      }
-    }
-
-    */ 
-
- // var slideout = new Slideout({
- //    'panel': document.getElementById('panel'),
- //    'menu': document.getElementById('menu'),
- //    'padding': 256,
- //    'tolerance': 70
- //  });
-    
-
-  $(window).scroll(function(){
-    if ($(window).scrollTop() >=91 ){
-       $('.header-wr').addClass('header-wr_sticky').removeClass('header-wr');
-    }
-    else {
-       $('.header-wr_sticky').addClass('header-wr').removeClass('header-wr_sticky');
-    }
-});
-
-/*
-$(".menu-link").click(function(){
-  
-  $("html").addClass("openNav");
-  
-});
-  
-$(".close-panel, section").click(function(){
-  
-$("html").removeClass("openNav");
-  
-});*/
-
-
 $(document).ready(function() {
-  $("[data-toggle]").click(function() {
-    var toggle_el = $(this).data("toggle");
-    $(toggle_el).toggleClass("open-sidebar");
-  });
+
+    // ================================ //
+    // Slideout Right Menu              //
+    // ================================ //
+
+    // Slideout Variable
+    var slideoutRight = new Slideout({
+        'panel': document.getElementById('panel'),
+        'panel': document.getElementById('panel'),
+        'menu': document.getElementById('m-menu'),
+        'padding': 256,
+        'tolerance': 70,
+        'side': 'right'
+    });
+
+       
+    // Toggle button
+    $('#toggle-button').click(function() {
+        slideoutRight.toggle();
+    });
+
+  slideoutRight.on('beforeopen', function() {
+        document.querySelector('.fixed').classList.add('fixed-open-right');
+    });
+
+    slideoutRight.on('beforeclose', function() {
+        document.querySelector('.fixed').classList.remove('fixed-open-right');
+    });
+
 });
