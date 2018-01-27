d5d092eed7b34c564a4995a35c15e2e0ea07a4a6
NiyazovProject <Niyazov_16@mail.ru>
2018-01-27 14:27:15 +0300

    first bounsy commit

---
+++ Bounsy/js/main.js
@@ -0,0 +1,46 @@
+//btn-adaptive-menu
+$(".btn_mnu").click(function() {
+	$(this).toggleClass("active");
+});
+
+	$(".btn_mnu").click(function() {
+		$(".nav").slideToggle(300, function(){
+			if($(this).css('display') === 'none'){
+				$(this).removeAttr('style');
+			}
+		});
+	});
+
+	//btn-adaptive-menu end
+
+//progress-bar
+$(function() {
+        $(".dial").knob({
+        	'cursor': 0,
+        	'width' : '140',
+        	'height' : '140',
+        	'readOnly' : true,
+        	'thickness' : 0.06,
+          'font' : 'OpenSans',
+        	'bgColor' : '#047378',
+        	'fgColor' : '#19bd9a',
+        	'format' : function (value) {
+        		return value + '%';
+        	}
+        });
+ });
+
+if($(document).scrollTop()>=$('.dial').scrollTop()+500){
+$(«.dial»).knob();
+$({animatedVal: 0}).animate({animatedVal: 60}, {
+duration: 2000,
+easing: «swing»,
+step: function() {
+$(«.dial»).val(Math.ceil(this.animatedVal)).trigger(«change»);
+}
+});
+
+}
+
+
+
