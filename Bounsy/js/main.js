aa7aecc6e8698029a7ba8d873d64310a1a5a50d1
NiyazovProject <Niyazov_16@mail.ru>
2018-02-02 16:07:29 +0300

    l.

--- Bounsy/js/main.js
+++ Bounsy/js/main.js
@@ -18,8 +18,36 @@ $(function() {
 
         $(".dial").knob({
         	'cursor': 0,
-        	'width' : '140',
-        	'height' : '140',
+        	'width' : '130',
+        	'height' : '130',
+        	'readOnly' : true,
+        	'thickness' : 0.08,
+          'font' : 'OpenSans',
+        	'bgColor' : '#047378',
+        	'fgColor' : '#19bd9a',
+        	'format' : function (value) {
+        		return value + '%';
+        	}
+        });
+
+        $(".dia2").knob({
+        	'cursor': 0,
+        	'width' : '130',
+        	'height' : '130',
+        	'readOnly' : true,
+        	'thickness' : 0.08,
+          'font' : 'OpenSans',
+        	'bgColor' : '#047378',
+        	'fgColor' : '#19bd9a',
+        	'format' : function (value) {
+        		return value + '%';
+        	}
+        });
+
+        $(".dia3").knob({
+        	'cursor': 0,
+        	'width' : '130',
+        	'height' : '130',
         	'readOnly' : true,
         	'thickness' : 0.08,
           'font' : 'OpenSans',
@@ -32,23 +60,84 @@ $(function() {
 
  });
 
-$(function() {
-	if($(document).scrollTop()>=$('.our-Services').scrollTop()+500){
-		$(".dial").knob();
-		$({animatedVal: 0}).animate({animatedVal: 70}, {
-			duration: 2000,
-			easing: "swing",
-			step: function() {
-				$(".dial").val(Math.ceil(this.animatedVal)).trigger("change");
-			}
-		});
+
+$(window).scroll(
+	function() {
+		var start = $(".our-services").offset().top -400;
+
+
+		if($(this).scrollTop() > start)
+        if ((($(this).scrollTop() > start)&($(this).scrollTop() < start+200))) {
+			var currentNumber = $(".dial").val();
+			$({numberValue: currentNumber}).animate({numberValue: 60}, {
+				duration: 3500,
+				easing: "linear",
+				step: function() {
+					$(".dial").val(Math.ceil(this.numberValue)).trigger("change");
+				}
+			});
+		}
+
+         
+        if($(this).scrollTop() > start)
+        if ((($(this).scrollTop() > start)&($(this).scrollTop() < start+200))) {
+            var currentNumber = $(".dia2").val();
+            $({numberValue: currentNumber}).animate({numberValue: 75}, {
+                duration: 3500,
+                easing: "linear",
+                step: function() {
+                    $(".dia2").val(Math.ceil(this.numberValue)).trigger("change");
+                }
+            });
+        }
+
+		
+        if($(this).scrollTop() > start)
+        if ((($(this).scrollTop() > start)&($(this).scrollTop() < start+200))) {
+            var currentNumber = $(".dia3").val();
+            $({numberValue: currentNumber}).animate({numberValue: 80}, {
+                duration: 3500,
+                easing: "linear",
+                step: function() {
+                    $(".dia3").val(Math.ceil(this.numberValue)).trigger("change");
+                }
+            });
+        }
+
 	}
+	);
+
+
+
+// slick-slider 
+
+ $(document).ready(function(){
+  $('.slick-slider').slick({
+     prevArrow: false,
+     nextArrow: false,
+     dots: true,
+  });
 });
 
 
 
 
 
+
+$(document).ready(function(){
+    $("#csc").on("click", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+ 
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+ 
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop:top}, 1500);
+ });
+});
 
 
   
